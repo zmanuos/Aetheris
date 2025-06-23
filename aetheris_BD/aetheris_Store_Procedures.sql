eb414e0d80e037596f649e462d88b9f980f73f55
Elihu-MR <164808048+Elihu-MR@users.noreply.github.com>
2025-06-22 19:29:55 -0700

    BD Cambios
    agregue lo siguiente:
    separe archivos las partes de la base de datos(base de datos, datos, store procedures y triggers) en archivos 
    
    Una tabla dedicada a la información del asilo
    relaciones corregidas
    campo para foto del residente
    cambie algunos campos para que sean mas correctos
    
    Store procedures:
    para ingresar familiares/empleados y que a la vez se registren sus usuarios para inicio de sesión
    
    Triggers:
    aumentar la cantidad de residentes/empleados al registrar uno
    cambiar cantidad de residentes/empleados al momento de que alguno cambie su estado a inactivo
    hasheo de contraseña de usuario

---
+++ aetheris_BD/aetheris_Store_Procedures.sql
@@ -0,0 +1,63 @@
+-- Registrar un nuevo empleado
+DELIMITER //
+CREATE PROCEDURE spRegistrarEmpleado (
+    IN nombre VARCHAR(100),
+    IN apellido VARCHAR(100),
+    IN fecha_nacimiento DATE,
+    IN genero VARCHAR(10),
+    IN telefono VARCHAR(15),
+    IN email VARCHAR(100),
+    IN contrasena VARCHAR(100)
+)
+BEGIN
+    DECLARE nuevo_id INT;
+    INSERT INTO PERSONAL (nombre, apellido, fecha_nacimiento, genero, telefono, activo)
+    VALUES (nombre, apellido, fecha_nacimiento, genero, telefono);
+
+    SET nuevo_id = LAST_INSERT_ID();
+
+    INSERT INTO USUARIO (usuario, contra, email, rol)
+    VALUES (
+        nuevo_id,
+        SHA2(contrasena, 256),
+        email,
+        2
+    );
+END;
+//
+DELIMITER ;
+
+
+-- Registrar un nuevo familiar
+DELIMITER //
+
+CREATE PROCEDURE spRegistrarFamiliar (
+    nombre VARCHAR(100),
+    apellido VARCHAR(100),
+    fecha_nacimiento DATE,
+    genero VARCHAR(10),
+    telefono VARCHAR(15),
+    id_residente INT,
+    id_parentesco INT,
+    email VARCHAR(100),
+    contrasena VARCHAR(100)
+)
+BEGIN
+    DECLARE nuevo_id INT;
+
+    INSERT INTO FAMILIAR (nombre, apellido, fecha_nacimiento, genero, telefono, id_residente, id_parentesco)
+    VALUES (nombre, apellido, fecha_nacimiento, genero, telefono, id_residente, id_parentesco);
+
+    SET nuevo_id = LAST_INSERT_ID();
+
+    INSERT INTO USUARIO (usuario, contra, email, rol)
+    VALUES (
+        nuevo_id,
+        SHA2(contrasena, 256),
+        email,
+        3
+    );
+END;
+//
+
+DELIMITER ;
