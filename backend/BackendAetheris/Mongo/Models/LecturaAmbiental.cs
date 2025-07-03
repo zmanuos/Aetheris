bb502ed2de1ac4031ef7978a4128c135cd846889
Elihu-MR <elihumoreno0@gmail.com>
2025-07-03 07:49:14 -0700

    Conexion con mongoDB   Models y controller para las colecciones   Lecturas_continuas_residente   Lecturas_ambientales   chequeos_semanales

---
+++ backend/BackendAetheris/Mongo/Models/LecturaAmbiental.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+public class LecturaAmbiental
+{
+    [BsonId]
+    public ObjectId Id { get; set; }
+
+    [BsonElement("zona")]
+    public string Zona { get; set; }
+
+    [BsonElement("dispositivoId")]
+    public string DispositivoId { get; set; }
+
+    [BsonElement("temperatura")]
+    public double Temperatura { get; set; }
+
+    [BsonElement("humedad")]
+    public double Humedad { get; set; }
+
+    [BsonElement("timestamp")]
+    public DateTime Timestamp { get; set; }
+}
