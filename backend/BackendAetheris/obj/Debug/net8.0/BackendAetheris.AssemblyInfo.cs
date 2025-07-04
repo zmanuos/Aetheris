509a499adbe3fe3936cf6c82783dbbd90c10ba11
Manuel Osuna <mmj.osuna@gmail.com>
2025-07-03 18:10:58 -0700

    Settings fix

--- backend/BackendAetheris/obj/Debug/net8.0/BackendAetheris.AssemblyInfo.cs
+++ backend/BackendAetheris/obj/Debug/net8.0/BackendAetheris.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendAetheris")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48ca8fc2b5a9ca668b345690df06812ad222d9c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a362f0059d38bc4396b0251b8641ae5d100b91b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendAetheris")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendAetheris")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
