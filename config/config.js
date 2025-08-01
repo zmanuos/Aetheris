2cc3f2f310b07fe5fd91aa86a9e4aaf15c1ff37c
AntonioB <antoniobarragan.h@gmail.com>
2025-08-01 11:55:55 -0700

    Resposive Home

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 //   ./config/config.js --  Configuracion Global
 const port = 5214;
-const ip = "172.18.5.81"; 
+const ip = "172.18.4.167"; 
 
 const Config = {
   API_BASE_URL: `http://${ip}:${port}/api`,
