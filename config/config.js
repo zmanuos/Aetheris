3b3267e968e43cabde3b16841af7083f27bfeb80
zmanuos <mmj.osuna@gmail.com>
2025-08-08 09:51:52 -0700

    Config

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 //   ./config/config.js --  Configuracion Global
 const port = 5214;
-const ip = "192.168.100.16"; 
+const ip = "192.168.47.73"; 
 
 const Config = {
   API_BASE_URL: `http://${ip}:${port}/api`,
